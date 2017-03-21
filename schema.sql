3cd9eaf83b3813fe3d7a359d1afc03aaa515557d
Melinda <melinda9794@gmail.com>
2017-03-20 21:28:30 -0400

    updated schema

--- schema.sql
+++ schema.sql
@@ -1,15 +1,19 @@
 CREATE DATABASE pokerDB;
 use pokerDB;
 
-CREATE TABLE player_info(
-playerID int primary key,
-player_name varchar(255) NOT NULL,
-weekly_scores int
-);
-
-CREATE TABLE league_info(
+CREATE TABLE users(
 playerID int primary key,
 username varchar(255) NOT NULL,
 password varchar(255) NOT NULL,
-real_name varchar(255) NOT NULL
+real_name varchar(255) NOT NULL,
+email varchar(255) NOT NULL,
+);
+
+CREATE TABLE tournament(
+tournament_name varchar(255) NOT NULL,
+tournament_ID int,
+username varchar(255),
+playerID int,
+ranking int,
+points int NULL
 );
