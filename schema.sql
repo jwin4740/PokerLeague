21d9d95e5d357d40f4ae5be154f5005c81bc9957
Ace <akhilark1088@gmail.com>
2017-04-01 17:07:06 -0400

    Adding admin user into production database. Minor UI editing.

--- schema.sql
+++ schema.sql
@@ -1,4 +1,3 @@
 CREATE DATABASE poker_DB;
 use poker_DB;
 
-INSERT INTO Users (email, username, hash, salt, role) VALUES ("admin@site.com", "admin28", "df5a2c7858b92215c16bb51aa5fb8acb74a92fad588451457a66681e2b8d0ffd3b058a13cbc2143bea24f40193f78e5241de60b185fcaeccd74af29dbb4f64f3", "4222f6ed79b06f63ad1e2282a0ebfa86", "admin");
